7258e66d1118715f5aa51bbbdcd6491b86a78aa8
Anne Douwe Bouma <annedouwe@sibben.nl>
2017-07-10 17:46:35 +0200

    Update

--- bot.js
+++ bot.js
@@ -7,12 +7,13 @@
 			game = window.game,
 			threshold = 200;
 		this.me = {};
-		this.deaths = 0;
-		this.isRespawning = false;
 		this.maxRpm = 0;
+		this.blocksize = 240;
 
 		// Settings
 		this.enabled = true;
+
+		$('body').append('<div id="cursor" style="position: absolute; width: 20px; height: 20px; background-color: #FF0000;"></div>');
 
 		/**
 		 * Function to spawn at start
@@ -40,8 +41,10 @@
 				type = 'mouseMoved';
 			}
 
-			var x = Math.min(Math.max(mouseX, 10), window.innerWidth - 10);
-			var y = Math.min(Math.max(mouseY, 10), window.innerHeight - 10);
+			var x = Math.min(Math.max(mouseX, 1), window.innerWidth - 1);
+			var y = Math.min(Math.max(mouseY, 1), window.innerHeight - 1);
+
+			$('#cursor').css('left', x + 'px').css('top', y + 'px');
 
 			if (type === 'mouseMoved') {
 				game.inputManager.emit('mouseUp', {
@@ -110,39 +113,102 @@
 		}
 
 		/**
+		 * Get food clusters in blocks
+		 */
+		this.getFoodBlocks = function() {
+			var allFood = _this.getFoodAround();
+			var foodBlocks = [];
+
+			_.each(allFood, function(food) {
+				if (food.x && food.y) {
+					var xBlock = Math.floor(food.x / _this.blocksize);
+					var yBlock = Math.floor(food.y / _this.blocksize);
+
+					if (!foodBlocks[xBlock]) {
+						foodBlocks[xBlock] = [];
+					}
+
+					if (!foodBlocks[xBlock][yBlock]) {
+						foodBlocks[xBlock][yBlock] = {
+							value: 0,
+						};
+					}
+
+					foodBlocks[xBlock][yBlock].value += food.reward;
+					foodBlocks[xBlock][yBlock].x = xBlock;
+					foodBlocks[xBlock][yBlock].y = yBlock;
+				}
+			});
+
+			foodBlocks = _.map(foodBlocks, function(foodBlockXs) {
+				return _.sortBy(foodBlockXs, function(foodBlockY) {
+					if (foodBlockY) {
+						return foodBlockY.value * -1;
+					}
+
+					return 100;
+				});
+			});
+
+			foodBlocks = _.sortBy(foodBlocks, function(foodBlockX) {
+				if (foodBlockX && foodBlockX[0]) {
+					return foodBlockX[0].value * -1;
+				}
+
+				return 100;
+			});
+
+			return foodBlocks;
+		}
+
+		this.playerIsClose = function(playerPos, mePos, offset) {
+			if ((playerPos.x >= (mePos.x - offset) || playerPos.x <= (mePos.x + offset)) && (playerPos.y >= (mePos.y - offset) || playerPos.y <= (mePos.y + offset))) {
+				return true;
+			}
+
+			return false;
+		}
+
+		this.ticks = 0;
+		/**
 		 * Main loop
 		 */
 		this.run = function() {
-			$('#playerList').html('');
-
 			var players = _this.getPlayersAround();
-			var food = _this.getFoodAround();
+
+			if (_this.ticks === 0 || _this.ticks % 60 === 0) {
+				_this.food = _this.getFoodBlocks();
+			}
+
 			var desired = {
 				type: 'mouseMoved',
 			}
 
-			if (players[0]) {
+			if (players[0] && players[0].rpm > 20) {
 				var player = players[0];
 
-				if (player.rpm > _this.me.rpm) {
+				if (player.rpm < (_this.me.rpm * 0.8)) {
+					desired.x = player.position.x;
+					desired.y = player.position.y;
+				} else if (player.rpm > _this.me.rpm) {
 					desired.x = (player.position.x * -1) + window.innerWidth;
 					desired.y = (player.position.y * -1) + window.innerHeight;
 				} else {
-					desired.x = player.position.x;
-					desired.y = player.position.y;
-					desired.type = 'mouseDown';
+					if (_this.food[0] && _this.food[0][0]) {
+						desired.x = ((_this.food[0][0].x * _this.blocksize));
+						desired.y = ((_this.food[0][0].y * _this.blocksize));
+					}
 				}
 			} else {
-				if (food[0]) {
-					desired.x = food[0].x;
-					desired.y = food[0].y;
+				if (_this.food[0] && _this.food[0][0]) {
+					desired.x = ((_this.food[0][0].x * _this.blocksize));
+					desired.y = ((_this.food[0][0].y * _this.blocksize));
 				}
 			}
 
 			if (_this.me.rpm > _this.maxRpm) {
 				_this.maxRpm = _this.me.rpm;
 			}
-
 
 			if (_this.me.rpm === 0) {
 				_this.respawn()
@@ -152,6 +218,7 @@
 				_this.sendMovement(desired.x, desired.y, desired.type);
 			}
 
+			_this.ticks++;
 			requestAnimationFrame(_this.run);
 		};
 
